7e086da5c1b379f12e7d8fe6e156e6c23455b729
Johannes J. Schmidt <schmidt@netzmerk.com>
2019-01-06 16:22:47 +0100

    use env var for port

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate iron;
 
+use std::env;
 use iron::prelude::*;
 use iron::status;
 
 fn main() {
+    let port = env::var("PORT").unwrap_or("8080".to_string());
+
     Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello world!"))))
-        .http("localhost:8080");
+        .http(format!("localhost:{}", port));
 }
