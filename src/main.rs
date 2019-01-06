3ddc8979ff4999550972e87cff6d6d8980013991
Johannes J. Schmidt <schmidt@netzmerk.com>
2019-01-06 16:05:15 +0100

    run on port 8080

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use iron::status;
 
 fn main() {
     Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello world!"))))
-        .http("localhost:80");
+        .http("localhost:8080");
 }
